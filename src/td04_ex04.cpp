e432ee787a0cdc7765f44c57bf8b5aeecd208dfc
Lilian Peuron <lilianpeuron@gmail.com>
2024-10-02 11:57:56 +0200

    td04_ex04 good

--- src/td04_ex04.cpp
+++ src/td04_ex04.cpp
@@ -3,35 +3,90 @@
 
 int main ()
 {
-    int level_stage {};
-    int ennemies {};
-    int level_ennemies {};
-    std::vector<int> vector {};
+    long int stage {};
+    long int number_enemies {};
+    std::vector<long int> level_ennemies {};
+    std::vector<long int> multiples_array {};
+    std::vector<long int> total_level_array {};
 
-    int total_energy {};
+    std::cout << "Entrez le niveau :" << std::endl;
+    std::cin >> stage ;
+    std::cout << "Niveau: " << stage << std::endl;
 
-    std::cout << "Saisissez le niveau terminé :" << std::endl;
-    std::cin >> level_stage ;
+    std::cout << "Entrez le nombre d'ennemies vaincus :" << std::endl;
+    std::cin >> number_enemies ;
+    std::cout << "Nombres d'ennemies: " << number_enemies << std::endl;
 
-    std::cout << "Saisissez le nombre d'ennemis vaincus :" << std::endl;
-    std::cin >> ennemies ;
-
-    std::cout << "Saisissez le niveau des ennemis vaincus :" << std::endl;
-
-    for (int i = 0; i < level_stage; i++)
+    for (int i = 0; i < number_enemies; i++)
     {
-        if (i % level_ennemies == 0)
+        long int level {};
+
+        std::cout << "Entrez le niveau de l'ennemie "<< i + 1 <<" :" << std::endl;
+        std::cin >> level ;
+        
+        level_ennemies.push_back(level); 
+    }
+
+    for (long int lvl_ennemy : level_ennemies)
+    {
+        std::cout << lvl_ennemy << ", ";
+    }
+
+    std::cout << std::endl ;
+    
+    for (int j = 0; j < level_ennemies.size() ; j++)
+    {
+        long int enemy_multiple = level_ennemies[j];
+
+        for (int k = 0;  enemy_multiple < stage; k++)
         {
-            vector.push_back(i);
+            enemy_multiple = level_ennemies[j] * (k + 1) ;
+            if (enemy_multiple < stage)
+            {
+                multiples_array.push_back(enemy_multiple);
+                std::cout << enemy_multiple << std::endl;
+            }
+        }
+    }
+    
+    for (long int multiple : multiples_array)
+    {
+        std::cout << multiple << ", ";
+    }
+
+    std::cout << std::endl;
+
+    for (int l = 0; l < multiples_array.size(); l++)
+    {
+        bool isPresent {false};
+        for ( int const thisLevel : total_level_array)
+        {
+            if (thisLevel == multiples_array[l])
+            {
+                isPresent = true;
+            }
+        }
+        if (isPresent == false)
+        {
+            total_level_array.push_back(multiples_array[l]);
         }
     }
 
-    for (int const value : vector)
+    for (int const thisLevel : total_level_array)
     {
-        total_energy += value;
+        std::cout << thisLevel << ", ";
     }
     
-    std::cout << "Exp obtenu :" << total_energy << std::endl;
-    
 
+    std::cout << std::endl;
+
+    long int exp_win {};
+    
+    for (long int value : total_level_array)
+    {
+        exp_win += value;
+    }
+    
+    std::cout << "Vous avez : " << exp_win << " exp" << std::endl;
+    
 }
